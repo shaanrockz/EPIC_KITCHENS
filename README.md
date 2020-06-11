f22ba4bfc09e96b19abe36b2daea3a6870f94057
Shahnawaz Alam <shahnawaz.alam.dev@gmail.com>
2020-06-11 18:25:40 +0200

    Adding config and updating Readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # EPIC-Kitchens
 TSN Base Code for EPIC Kitchen
+
+
+python -W ignore ../main.py --config ../configs/config_seen.yaml
--- config.py
+++ config.py
@@ -2,58 +2,24 @@ import yaml
 
 
 class config:
-    def __init__(self, external_path=None, debugging=False, additionalText='sample'):
+    def __init__(self, external_path, debugging=False, additionalText='sample'):
 
-        if external_path:
-            stream = open(external_path, "r")
-            docs = yaml.load_all(stream)
-            for doc in docs:
-                for k, v in doc.items():
-                    #if k == "train":
-                    for k1, v1 in v.items():
-                        cmd = "self." + k1 + "=" + repr(v1)
-                        print(cmd)
-                        exec(cmd)
-                        # self.__setattr__(k1, repr(v1))
-            stream.close()
-            if hasattr(self, 'train_mode'):
-                assert self.train_mode in ["train-seen", "train-unseen", "val-seen", "val-unseen", "whole-train", "test"]
-            if hasattr(self, 'val_mode'):
-                assert self.val_mode in ["train-seen", "train-unseen", "val-seen", "val-unseen", "whole-train", "test"]
-        else:
-            self.manualSeed = 786
-            self.epoch = 80
+        stream = open(external_path, "r")
+        docs = yaml.load_all(stream)
+        for doc in docs:
+            for k, v in doc.items():
+                #if k == "train":
+                for k1, v1 in v.items():
+                    cmd = "self." + k1 + "=" + repr(v1)
+                    print(cmd)
+                    exec(cmd)
+                    # self.__setattr__(k1, repr(v1))
+        stream.close()
+        if hasattr(self, 'train_mode'):
+            assert self.train_mode in ["train-seen", "train-unseen", "val-seen", "val-unseen", "whole-train", "test"]
+        if hasattr(self, 'val_mode'):
+            assert self.val_mode in ["train-seen", "train-unseen", "val-seen", "val-unseen", "whole-train", "test"]
 
-            # Dataset Config
-            self.anno_path = "/media/data/salam/epic-kitchens/annotations/EPIC_train_action_labels.csv"
-            self.data_path = "/media/data/salam/epic-kitchens/EPIC_KITCHENS_2018/frames_rgb_flow/rgb/train/"
-            self.train_mode = "train-seen"
-            self.val_mode = "val-seen"
-            
-            if hasattr(self, 'train_mode'):
-                assert self.train_mode in ["train-seen", "train-unseen", "val-seen", "val-unseen", "whole-train", "test"]
-            if hasattr(self, 'val_mode'):
-                assert self.val_mode in ["train-seen", "train-unseen", "val-seen", "val-unseen", "whole-train", "test"]
-            self.val_seen_vids = [
-                "P01_18",
-                "P02_09",
-                "P03_27",
-                "P04_04",
-                "P07_07",
-                "P08_20",
-                "P15_01",
-                "P22_08",
-                "P24_03",
-                "P25_01",
-                "P26_05",
-                "P28_02",
-                "P30_04",
-                "P31_04",
-            ]
-
-            # Model Config
-            self.n_segments = 8
-            self.algo = "MTGA"
 
         self.lr = 0.01
         self.momentum = 0.9
